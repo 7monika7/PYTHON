ca57396db65d9343dbe8be27da332ff87b07e03a
7monika7 <monasomashekara@gmail.com>
2025-03-10 15:48:37 +0530

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,6 @@
+#simple interest
+p=int(input("enter the principle amt"))
+t=int(input("enter the time in years"))
+r=float(input("enter the rate of interest per annum"))
+si=(p*t*r)/100
+print("the simple interest is :",si)
