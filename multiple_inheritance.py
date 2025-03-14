a7e769dedc73f34d06d94abf65daea4adb69dc1e
7monika7 <monasomashekara@gmail.com>
2025-03-14 12:00:19 +0530

    Create multiple_inheritance.py

---
+++ multiple_inheritance.py
@@ -0,0 +1,13 @@
+class annie:
+  def ex1(self):
+    print("annie is a poor girll")
+class aziel:
+  def ex2(self):
+    print("aziel is a hacker girl")
+class fiora(annie,aziel):
+  def ex3(self):
+    print("fiora and aziel are sister and they are unknown of this truth")
+obj=fiora()
+obj.ex1()
+obj.ex2()
+obj.ex3()
