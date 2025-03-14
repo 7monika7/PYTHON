38042cab89c6d340fc114109e7f09b7a1ba9d68c
7monika7 <monasomashekara@gmail.com>
2025-03-14 10:42:18 +0530

    Create encapsulation1.py

---
+++ encapsulation1.py
@@ -0,0 +1,9 @@
+class mouna:
+  def  __init__(self):
+    self.appu="hi babe!"
+    self.__dummy="you are sick!"
+  def mona(self):
+    print(self.__dummy)
+obj=mouna()
+print(obj.appu)
+obj.mona()
